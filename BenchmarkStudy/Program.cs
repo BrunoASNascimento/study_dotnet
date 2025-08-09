8bdd75ef213ec33427ec2f01ffcf0984ac560373
BrunoASNascimento <bruno-asn@outlook.com>
2025-08-09 20:04:15 -0300

    Add initial project files and benchmarking setup

---
+++ BenchmarkStudy/Program.cs
@@ -0,0 +1,48 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace BenchmarkStudy;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<MyBenchmarks>();
+    }
+}
+
+public class MyBenchmarks
+{
+    private int[] numbers;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        numbers = Enumerable.Range(1, 1000).ToArray();
+    }
+
+    [Benchmark]
+    public int SumWithForLoop()
+    {
+        int sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+            sum += numbers[i];
+        return sum;
+    }
+
+    [Benchmark]
+    public int SumWithForEach()
+    {
+        int sum = 0;
+        foreach (int i in numbers) 
+            sum += i;
+        
+        return sum;
+    }
+
+    [Benchmark]
+    public int SumWithLinq()
+    {
+        return numbers.Sum();
+    }
+}
