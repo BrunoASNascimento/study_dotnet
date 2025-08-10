36600f510b7ccc8c8f4bb500ac529776c4cd223f
BrunoASNascimento <bruno-asn@outlook.com>
2025-08-10 01:25:40 -0300

    Add benchmarking methods for sum calculations

--- BenchmarkStudy/Program.cs
+++ BenchmarkStudy/Program.cs
@@ -1,5 +1,7 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+using BenchmarkStudy.Services;
+
 
 namespace BenchmarkStudy;
 
@@ -7,42 +9,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        BenchmarkRunner.Run<MyBenchmarks>();
-    }
-}
-
-public class MyBenchmarks
-{
-    private int[] numbers;
-
-    [GlobalSetup]
-    public void Setup()
-    {
-        numbers = Enumerable.Range(1, 1000).ToArray();
-    }
-
-    [Benchmark]
-    public int SumWithForLoop()
-    {
-        int sum = 0;
-        for (int i = 0; i < numbers.Length; i++)
-            sum += numbers[i];
-        return sum;
-    }
-
-    [Benchmark]
-    public int SumWithForEach()
-    {
-        int sum = 0;
-        foreach (int i in numbers) 
-            sum += i;
-        
-        return sum;
-    }
-
-    [Benchmark]
-    public int SumWithLinq()
-    {
-        return numbers.Sum();
+        BenchmarkRunner.Run<BenchmarkSum>();
     }
 }
